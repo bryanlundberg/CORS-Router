d922dfddb8f22b74a07090658ccb5374ca2398b8
Bryan Lundberg <bryanlundberg@outlook.com>
2023-08-30 15:35:59 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/:url(*)", async function (req, res, next) {
+  const apiUrl = req.params.url;
+  try {
+    const response = await axios.get(apiUrl);
+    res.json({ data: response.data });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+module.exports = app;
