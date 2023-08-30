74726dbf9f678fb6ec5b597c90c9ba1b11cc6af4
Bryan Lundberg <bryanlundberg@outlook.com>
2023-08-30 16:13:43 -0600

    Improve response handle

--- app.js
+++ app.js
@@ -14,11 +14,20 @@ app.use(cookieParser());
 
 app.get("/:url(*)", async function (req, res, next) {
   const apiUrl = req.params.url;
+
   try {
     const response = await axios.get(apiUrl);
-    res.json(response.data);
+    res.status(response.status);
+    res.set(response.headers);
+    res.send(response.data);
   } catch (error) {
-    res.json(error);
+    if (error.response) {
+      res.status(error.response.status);
+      res.set(error.response.headers);
+      res.send(error.response.data);
+    } else {
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
   }
 });
 
