3db4f8a11c1935fb739913d801ce7535136c5806
rojonaitor <bryanlundberg@outlook.com>
2023-09-05 21:19:28 -0600

    add landing page

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const axios = require("axios");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.get("/", function (req, res, next) {
+  res.render("index");
+});
 
 app.get("/proxy/*", async function (req, res, next) {
   let apiUrl = req.originalUrl.replace("/proxy/", "");
