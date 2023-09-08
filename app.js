4d3f2c7b638e2a60ba875dc1abf898f73bc4b2ba
rojonaitor <bryanlundberg@outlook.com>
2023-09-07 18:02:47 -0600

    improve landing page and other minor changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
